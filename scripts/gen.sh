4bd4d2a58a824c60e5a8078e89005b51f44897d8
Márton Visnovitz <vimtaai@inf.elte.hu>
2020-02-20 16:36:26 +0100

    Captcha hozzáadása

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -8,5 +8,5 @@ pandoc \
     --slide-level=2 \
     --template=templates/template.html \
     --variable revealjs-version="3.9.2" \
-    --variable stylesheet-version="1.0.8" \
+    --variable stylesheet-version="1.0.11" \
     $1/slides.md > $1/index.html
